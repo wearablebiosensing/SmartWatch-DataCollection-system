1cbe8dbd99163d74da014cf453f0a5ea19fe302d
sadhushehjar <45221445+sadhushehjar@users.noreply.github.com>
2025-08-17 23:25:30 -0400

    galaxy studio app test update MQTT streams

--- v3.0.0/companion_studio/src/static/js/graph.js
+++ v3.0.0/companion_studio/src/static/js/graph.js
@@ -1,7 +1,6 @@
 var socket = io.connect("http://" + document.domain + ":" + location.port);
-console.log(" SOCKET CONNECTION:==== ",document.domain);
-console.log(" SOCKET CONNECTION:==== ",document.data);
-console.log(" SOCKET CONNECTION:==== ",location.port);
+console.log(" SOCKET CONNECTION:==== ", document.domain);
+console.log(" SOCKET CONNECTION:==== ", location.port);
 
 const maxDataPoints = 300; // Maximum data points to display on each chart
 
@@ -33,68 +32,159 @@ function initializeChart(canvasId, maxDataPoints) {
 
     data: {
       labels: [],
-      datasets: [
-        {
-          label: `${canvasId}`,
-          data: [],
-          borderWidth: 1,
-          borderColor: "#0A1572", // Chart line color
-          backgroundColor: "transparent",
-        },
-      ],
+      datasets: [{
+        label: `${canvasId}`,
+        data: [],
+        borderWidth: 1,
+        borderColor: "#0A1572",
+        backgroundColor: "transparent",
+      }, ],
     },
     options: {
       scales: {
         x: {
           ticks: {
-            color: "#0A1572", // Tick label color
+            color: "#0A1572",
           },
           grid: {
             drawOnChartArea: false,
           },
           display: true,
           ticks: {
-            display: false,
-          },
+            display: false
+          }
         },
         y: {
-          grid: {
-            color: "#0A1572",
-          },
           ticks: {
             color: "#0A1572",
           },
-        },
-      },
-      legend: {
-        labels: {
-          fontColor: "#0A1572", // Adjust legend label color for dark theme
-        },
-      },
-      elements: {
-        line: {
-          tension: 0.4,
-        },
-        point: {
-          radius: 0,
-        },
-      },
-    },
+          grid: {
+            color: "rgba(0, 0, 0, 0.1)"
+          }
+        }
+      }
+    }
   });
 
-  currentListener = "mqtt_data_" + canvasId;
-  socket.on(currentListener, function (msg) {
-    var time = new Date().toLocaleTimeString();
+  // Dynamically create the event name based on the canvasId and the watchID from local storage.
+  const watchID = localStorage.getItem("carewear_watch_id");
+  const eventName = `mqtt_data_${canvasId}_${watchID}`;
+  currentListener = eventName;
+
+  socket.on(eventName, function(data) {
     if (chart.data.labels.length >= maxDataPoints) {
-      chart.data.labels.shift(); // Remove the oldest label
+      chart.data.labels.shift();
       chart.data.datasets.forEach((dataset) => {
-        dataset.data.shift(); // Remove the oldest data point
+        dataset.data.shift();
       });
     }
-    chart.data.labels.push(time);
-    chart.data.datasets.forEach((dataset) => {
-      dataset.data.push(msg.data);
-    });
+
+    // Add the new data point to the chart
+    chart.data.labels.push(""); // Add an empty label
+    chart.data.datasets[0].data.push(data["data"]); // Use the "data" key from the emitted dictionary
+
+    // Redraw the chart
     chart.update();
   });
 }
+
+// var socket = io.connect("http://" + document.domain + ":" + location.port);
+// console.log(" SOCKET CONNECTION:==== ",document.domain);
+// console.log(" SOCKET CONNECTION:==== ",document.data);
+// console.log(" SOCKET CONNECTION:==== ",location.port);
+
+// const maxDataPoints = 300; // Maximum data points to display on each chart
+
+// let chart = null;
+// let currentListener = null;
+// const gGraphDropdownInput = document.getElementById("graphDropdownInput");
+// gGraphDropdownInput.addEventListener("change", () => {
+//   if (chart) {
+//     chart.destroy();
+//   }
+//   if (currentListener) {
+//     socket.off(currentListener);
+//   }
+
+//   initializeChart(gGraphDropdownInput.value, maxDataPoints);
+// });
+
+// function setupCharts() {
+//   // Initialize charts for each canvas
+//   initializeChart("accelerometer", maxDataPoints);
+// }
+// setupCharts();
+
+// function initializeChart(canvasId, maxDataPoints) {
+//   var ctx = document.getElementById("chart").getContext("2d");
+
+//   chart = new Chart(ctx, {
+//     type: "line",
+
+//     data: {
+//       labels: [],
+//       datasets: [
+//         {
+//           label: `${canvasId}`,
+//           data: [],
+//           borderWidth: 1,
+//           borderColor: "#0A1572", // Chart line color
+//           backgroundColor: "transparent",
+//         },
+//       ],
+//     },
+//     options: {
+//       scales: {
+//         x: {
+//           ticks: {
+//             color: "#0A1572", // Tick label color
+//           },
+//           grid: {
+//             drawOnChartArea: false,
+//           },
+//           display: true,
+//           ticks: {
+//             display: false,
+//           },
+//         },
+//         y: {
+//           grid: {
+//             color: "#0A1572",
+//           },
+//           ticks: {
+//             color: "#0A1572",
+//           },
+//         },
+//       },
+//       legend: {
+//         labels: {
+//           fontColor: "#0A1572", // Adjust legend label color for dark theme
+//         },
+//       },
+//       elements: {
+//         line: {
+//           tension: 0.4,
+//         },
+//         point: {
+//           radius: 0,
+//         },
+//       },
+//     },
+//   });
+
+//   currentListener = "mqtt_data_" + canvasId;
+//   socket.on(currentListener, function (msg) {
+//     var time = new Date().toLocaleTimeString();
+//     if (chart.data.labels.length >= maxDataPoints) {
+//       chart.data.labels.shift(); // Remove the oldest label
+//       chart.data.datasets.forEach((dataset) => {
+//         dataset.data.shift(); // Remove the oldest data point
+//       });
+//     }
+//     chart.data.labels.push(time);
+//     chart.data.datasets.forEach((dataset) => {
+//       dataset.data.push(msg.data);
+//     });
+//     chart.update();
+//   });
+// }
